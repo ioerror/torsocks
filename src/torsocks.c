3ef7742de9dbc5382135bf4ce3bc8c7809d75225
Matthew Finkel <Matthew.Finkel@gmail.com>
2013-04-13 22:53:12 +0000

    Add definition and placeholder for new routines

--- src/torsocks.c
+++ src/torsocks.c
@@ -1106,3 +1106,69 @@ ssize_t torsocks_sendmsg_guts(SENDMSG_SIGNATURE, ssize_t (*original_sendmsg)(SEN
     return (ssize_t) original_sendmsg(s, msg, flags);
 }
 
+/* TODO Add MMSG_AVAILABLE */
+#ifdef SENDMMSG_AVAILABLE
+int torsocks_sendmmsg_guts(SENDMMSG_SIGNATURE, int (*original_sendmmsg)(SENDMMSG_SIGNATURE))
+{
+    return original_sendmmsg(s, msgvec, vlen, flags);
+}
+#endif /* SENDMMSG_AVAILABLE */
+
+#ifdef RECVMMSG_AVAILABLE
+int torsocks_recvmmsg_guts(RECVMMSG_SIGNATURE, int (*original_recvmmsg)(RECVMMSG_SIGNATURE))
+{
+    return original_recvmmsg(s, msgvec, vlen, flags, timeout);
+}
+#endif /* RECVMMSG_AVAILABLE */
+
+ssize_t torsocks_recvfrom_guts(RECVFROM_SIGNATURE, ssize_t(*original_recvfrom)(RECVFROM_SIGNATURE))
+{
+    return (ssize_t) original_recvfrom(s, buf, len, flags, addr, addr_len);
+}
+
+ssize_t torsocks_recvmsg_guts(RECVMSG_SIGNATURE, ssize_t(*original_recvmsg)(RECVMSG_SIGNATURE))
+{
+    return (ssize_t) original_recvmsg(s, msg, flags);
+}
+
+ssize_t torsocks_write_guts(WRITE_SIGNATURE, ssize_t(*original_write)(WRITE_SIGNATURE))
+{
+    return original_write(fd, buf, count);
+}
+
+ssize_t torsocks_read_guts(READ_SIGNATURE, ssize_t(*original_read)(READ_SIGNATURE))
+{
+    return original_read(fd, buf, count);
+}
+
+ssize_t torsocks_send_guts(SEND_SIGNATURE, ssize_t(*original_send)(SEND_SIGNATURE))
+{
+    return original_send(fd, buf, count, flags);
+}
+
+ssize_t torsocks_recv_guts(RECV_SIGNATURE, ssize_t(*original_recv)(RECV_SIGNATURE))
+{
+    return original_recv(fd, buf, count, flags);
+}
+
+ssize_t torsocks_readv_guts(READV_SIGNATURE, ssize_t(*original_readv)(READV_SIGNATURE))
+{
+    return original_readv(fd, iov, iovcnt);
+}
+
+ssize_t torsocks_writev_guts(WRITEV_SIGNATURE, ssize_t(*original_writev)(WRITEV_SIGNATURE))
+{
+    return original_writev(fd, iov, iovcnt);
+}
+
+#if PPOLL_AVAILABLE
+int torsocks_ppoll_guts(PPOLL_SIGNATURE, int(*original_ppoll)(PPOLL_SIGNATURE))
+{
+    return original_ppoll(fds, nfds, timeout, sigmask);
+}
+#endif /* PPOLL_AVAILABLE */
+
+int torsocks_pselect_guts(PSELECT_SIGNATURE, int(*original_pselect)(PSELECT_SIGNATURE))
+{
+    return original_pselect(nfds, readfds, writefds, exceptfds, timeout, sigmask);
+}
